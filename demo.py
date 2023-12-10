926c0ca527e1da2f1187dff46105829403573687
Name <kasusaland@gmail.com>
2023-12-10 15:19:57 +0800

    update first time

---
+++ demo.py
@@ -0,0 +1,8 @@
+from console_color_writer import *
+
+print_green('ok','some info text ...')
+print_yellow('alert','some info text ...')
+print_red('bad','some info text ...')
+print_cyan('some info text ... only cyan')
+print_magenta('some info text ... only magenta')
+print_white('only white text here (super white)')
